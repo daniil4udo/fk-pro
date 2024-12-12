9ebd6ea9b5e0bea15d6d51fa883d9187cfa30d74
daniil4udo <daniil4udo@gmail.com>
2024-12-12 07:29:25 +0400

    v0.127.10

--- init-pro.sh
+++ init-pro.sh
@@ -1,6 +1,6 @@
 rm -rf node_modules lib/ pro/
 pnpm install
-mv node_modules/.pnpm/@formkit+pro@0.127.9_@formkit+core@1.6.9_@formkit+inputs@1.6.9_@formkit+utils@1.6.9/node_modules/@formkit/pro .
+mv node_modules/.pnpm/@formkit+pro@0.127.10_@formkit+core@1.6.9_@formkit+inputs@1.6.9_@formkit+utils@1.6.9/node_modules/@formkit/pro .
 mv pro/ lib/
 rm -rf lib/README.md lib/LICENSE.md lib/index.cjs lib/package.json
 pnpm exec prettier ./lib/ --write
